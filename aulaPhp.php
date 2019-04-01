fef0ac5115ff8d2970d5b9f979022f18fc495b5d
MATHEUS RIBEIRO GARCIA <144151009@labinfo.unifacs.br>
2019-04-01 20:43:01 -0300

    Básico php

---
+++ aulaPhp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Exemplo PHP</title>
+</head>
+
+<body>
+   <?php
+        $name = "Pedro";
+        $age = "30";
+        echo "Meus Dados <br> <b>Nome:</b> $name <br><b>Idade:</b> $age";
+    ?>
+</body>
+
+</html>
